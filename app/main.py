c4891538a3c960da537e3f7e002a03a7b80b9450
Dogukan Bakibaba <mehmet.bakibaba@gmail.com>
2026-05-13 15:34:46 -0400

    feat: add Haversine distance function and find-washroom endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from database import init_db,seed_db
+from pydantic import BaseModel
+from distance import haversine
+from database import get_connection, init_db, seed_db
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -14,3 +16,32 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/")
 def root():
     return {"message":"Pitstop API is running "}
+
+class LocationInput(BaseModel):
+    latitude:float
+    longitude:float
+
+@app.post("/find-washroom")
+def find_washroom(location: LocationInput):
+    conn=get_connection()
+    washrooms=conn.execute("SELECT*FROM washrooms").fetchall()
+    conn.close()
+    
+    results=[]
+    for w in washrooms:
+        dist=haversine(location.latitude,location.longitude,
+        w["latitude"],w["longitude"])
+        results.append({
+
+             "id": w["id"],
+            "name": w["name"],
+            "address": w["address"],
+            "distance_km": round(dist, 2),
+            "is_open24h": w["is_open24h"],
+            "is_accessible": w["is_accessible"],
+            "comments": w["comments"]
+        })
+
+    results.sort(key=lambda x: x["distance_km"])
+    return results[:3]
+    
